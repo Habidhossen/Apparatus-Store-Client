bda8ab55244b80f346de03dbef4c618014c0ff6c
Habidhossen <habidhossen2@gmail.com>
2022-05-22 22:31:23 +0600

    react hook form added

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mx-auto mt-12">
@@ -9,36 +17,52 @@ const Login = () => {
           <h1 className="text-center text-xl font-bold mb-4">
             Login to your account
           </h1>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email</span>
-            </label>
-            <input
-              type="text"
-              placeholder="email"
-              class="input input-bordered"
-            />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Password</span>
-            </label>
-            <input
-              type="text"
-              placeholder="password"
-              class="input input-bordered"
-            />
-            <label class="text-sm mt-3">
-              Don't have an account?
-              <Link className="btn-link" to="/signup">
-                {" "}
-                Register
-              </Link>
-            </label>
-          </div>
-          <div class="form-control mt-6">
-            <button class="btn btn-primary">Login</button>
-          </div>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                class="input input-bordered"
+                {...register("email", { required: true })}
+              />
+              {errors.email && (
+                <span class="label-text-alt text-red-500 mt-2">
+                  Email is required
+                </span>
+              )}
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                class="input input-bordered"
+                {...register("password", { required: true })}
+              />
+              {errors.password && (
+                <span class="label-text-alt text-red-500 mt-2">
+                  Password is required
+                </span>
+              )}
+
+              <label class="text-sm mt-3">
+                Don't have an account?
+                <Link className="btn-link" to="/signup">
+                  {" "}
+                  Register
+                </Link>
+              </label>
+            </div>
+            <div class="form-control mt-6">
+              <button class="btn btn-primary">Login</button>
+            </div>
+          </form>
           <div class="divider">OR</div>
           <button class="btn btn-outline btn-secondary">
             Continue with Google
