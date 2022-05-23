f1bc3d4e813192796bde8c1bee7377d438100385
Habidhossen <habidhossen2@gmail.com>
2022-05-23 19:39:45 +0600

    server added

--- src/pages/Home/Tools/Tools.js
+++ src/pages/Home/Tools/Tools.js
@@ -1,69 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Tool from "../Tool/Tool";
 
 const Tools = () => {
-  const Tools = [
-    {
-      _id: 1,
-      name: "Hammer",
-      img: "https://tolsen.com.ph/wp-content/uploads/2021/03/products-25158-25160.jpg",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, rerum?",
-      price: "500",
-      minimumOrderQuantity: 20,
-      availableQuantity: 50,
-    },
-    {
-      _id: 2,
-      name: "Hammer",
-      img: "https://tolsen.com.ph/wp-content/uploads/2021/03/products-25158-25160.jpg",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, rerum?",
-      price: "500",
-      minimumOrderQuantity: 20,
-      availableQuantity: 50,
-    },
-    {
-      _id: 3,
-      name: "Hammer",
-      img: "https://tolsen.com.ph/wp-content/uploads/2021/03/products-25158-25160.jpg",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, rerum?",
-      price: "500",
-      minimumOrderQuantity: 20,
-      availableQuantity: 50,
-    },
-    {
-      _id: 4,
-      name: "Hammer",
-      img: "https://tolsen.com.ph/wp-content/uploads/2021/03/products-25158-25160.jpg",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, rerum?",
-      price: "500",
-      minimumOrderQuantity: 20,
-      availableQuantity: 50,
-    },
-    {
-      _id: 5,
-      name: "Hammer",
-      img: "https://tolsen.com.ph/wp-content/uploads/2021/03/products-25158-25160.jpg",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, rerum?",
-      price: "500",
-      minimumOrderQuantity: 20,
-      availableQuantity: 50,
-    },
-    {
-      _id: 6,
-      name: "Hammer",
-      img: "https://tolsen.com.ph/wp-content/uploads/2021/03/products-25158-25160.jpg",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, rerum?",
-      price: "500",
-      minimumOrderQuantity: 20,
-      availableQuantity: 50,
-    },
-  ];
+  const [tools, setTools] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:5000/product";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setTools(data));
+  }, []);
 
   return (
     <section className="my-20">
       <h1 className="text-4xl font-extrabold text-center mb-6">Our Tools</h1>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-16">
-        {Tools.map((singleTool) => (
+        {tools.map((singleTool) => (
           <Tool key={singleTool._id} singleTool={singleTool} />
         ))}
       </div>
