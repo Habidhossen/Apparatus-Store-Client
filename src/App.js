acdbd6d9be040dc5d6efb2f21638a2cc9de38ed6
Habidhossen <habidhossen2@gmail.com>
2022-05-23 15:52:09 +0600

    nested route added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AddReview from "./pages/Dashboard/AddReview/AddReview";
+import Dashboard from "./pages/Dashboard/Dashboard/Dashboard";
+import MyOrder from "./pages/Dashboard/MyOrder/MyOrder";
+import MyProfile from "./pages/Dashboard/MyProfile/MyProfile";
 import Home from "./pages/Home/Home/Home";
 import Purchase from "./pages/Home/Purchase/Purchase";
 import Login from "./pages/Login/Login/Login";
@@ -15,6 +19,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/purchase/:Id" element={<Purchase />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<MyProfile />} />
+          <Route path="order" element={<MyOrder />} />
+          <Route path="review" element={<AddReview />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/pages/Dashboard/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard/Dashboard.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div class="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content bg-gray-50">
+        {/* <!-- Page content here --> */}
+        <h1 className="text-3xl font-bold text-gray-800 mt-2 ml-2">
+          Dashboard
+        </h1>
+        <Outlet />
+        <label
+          for="my-drawer-2"
+          class="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div class="drawer-side">
+        <label for="my-drawer-2" class="drawer-overlay"></label>
+        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+          <li>
+            <Link to="">My Profile</Link>
+          </li>
+          <li>
+            <Link to="order">My Order</Link>
+          </li>
+          <li>
+            <Link to="review">Add a Review</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
