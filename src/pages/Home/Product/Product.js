29c0824a3e51ba59f39d3a0a835e19fb09d60240
Habidhossen <habidhossen2@gmail.com>
2023-02-28 16:47:31 +0600

    products section updated

--- src/pages/Home/Product/Product.js
+++ src/pages/Home/Product/Product.js
@@ -11,28 +11,30 @@ const Product = ({ singleProduct }) => {
     minimumOrderQuantity,
     availableQuantity,
   } = singleProduct;
+
   return (
-    <div className="card card-compact bg-base-100 shadow-xl">
-      <figure className="p-8">
-        <img src={img} alt="" />
+    <div className="card card-compact bg-base-100 shadow-sm rounded-md">
+      <figure className="p-4">
+        <img
+          className="w-full transition duration-300 hover:scale-110"
+          src={img}
+          alt=""
+        />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>{desc}</p>
-        <h3>
-          Price: <span className="font-semibold">${price}</span> (Per unit
-          price)
-        </h3>
-        <h3>
-          Available Quantity:{" "}
-          <span className="font-semibold">{availableQuantity}</span>
-        </h3>
-        <h3>
-          Minimum Order Quantity:{" "}
-          <span className="font-semibold">{minimumOrderQuantity}</span>
-        </h3>
-        <div className="card-actions justify-end">
-          <Link to={`/purchase/${_id}`} className="btn btn-primary capitalize">
+
+      <div className="card-body space-y-1">
+        <div className="flex items-center gap-3">
+          <del className="text-md text-text">${Number(price) + 100}</del>
+          <h6 className="text-xl font-semibold text-primary">${price}</h6>
+        </div>
+        <h2 className="text-md font-bold text-heading">{name}</h2>
+        <p className="text-sm text-text">{desc.slice(0, 100) + "..."}</p>
+
+        <div className="card-actions">
+          <Link
+            to={`/purchase/${_id}`}
+            className="flex items-center justify-center gap-2 w-full bg-primary hover:bg-rose-600 transition duration-150 text-white text-xs font-semibold py-2 rounded"
+          >
             Buy Now
           </Link>
         </div>
--- src/pages/Home/Products/Products.js
+++ src/pages/Home/Products/Products.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
 import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
 import Loading from "../../Shared/Loading/Loading";
 import Tool from "../Product/Product";
 
@@ -17,9 +19,22 @@ const Products = () => {
   }
 
   return (
-    <section className="my-20">
-      <h1 className="text-4xl font-extrabold text-center mb-6">Our Tools</h1>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mx-16">
+    <section className="my-20 mx-20">
+      <div className="flex justify-between my-4">
+        <div>
+          <h1 className="text-xl font-bold">Featured Products</h1>
+        </div>
+        <div>
+          <Link
+            to="/"
+            className="flex items-center gap-2 text-sm font-bold hover:text-primary transition duration-300"
+          >
+            View All <FaLongArrowAltRight />
+          </Link>
+        </div>
+      </div>
+      <hr className="mb-5" />
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {products
           .map((singleProduct) => (
             <Tool key={singleProduct._id} singleProduct={singleProduct} />
