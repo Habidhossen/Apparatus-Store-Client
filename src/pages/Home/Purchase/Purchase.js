3222a934a70a14330c2d810009e75504721711c1
Habidhossen <habidhossen2@gmail.com>
2023-03-01 03:08:15 +0600

    small update

--- src/pages/Home/Purchase/Purchase.js
+++ src/pages/Home/Purchase/Purchase.js
@@ -248,21 +248,42 @@ const Purchase = () => {
       >
         <input type="checkbox" />
         <div className="collapse-title text-md font-semibold text-heading">
-          Refund Policy
+          Shipping Information
         </div>
         <div className="collapse-content">
           <p className="text-sm text-text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
-            libero cupiditate corporis cumque cum, nulla quis consectetur amet
-            in aut, magnam maxime iste velit voluptatum odio fuga. Odio ut ab
-            facere libero minus quasi ipsum hic aliquam itaque, deserunt
-            obcaecati perspiciatis illum? Dolore placeat tempore quis possimus
-            magnam, molestiae excepturi! Asperiores harum ullam quod explicabo
-            necessitatibus suscipit aliquid maiores saepe ducimus quos. Quisquam
-            incidunt sapiente error voluptas sit! Ut molestiae laboriosam quam
-            eaque perspiciatis, eligendi earum itaque illo in illum, odit quasi
-            sequi dolor debitis delectus blanditiis nobis totam culpa. Quasi,
-            porro pariatur. Perferendis, a! Id quibusdam vero veniam cupiditate.
+            <b>Returns Policy</b>
+            <br />
+            You may return most new, unopened items within 30 days of delivery
+            for a full refund. We'll also pay the return shipping costs if the
+            return is a result of our error (you received an incorrect or
+            defective item, etc.). You should expect to receive your refund
+            within four weeks of giving your package to the return shipper,
+            however, in many cases you will receive a refund more quickly. This
+            time period includes the transit time for us to receive your return
+            from the shipper (5 to 10 business days), the time it takes us to
+            process your return once we receive it (3 to 5 business days), and
+            the time it takes your bank to process our refund request (5 to 10
+            business days). If you need to return an item, simply login to your
+            account, view the order using the 'Complete Orders' link under the
+            My Account menu and click the Return Item(s) button. We'll notify
+            you via e-mail of your refund once we've received and processed the
+            returned item.
+            <br />
+            <br />
+            <b>Shipping</b>
+            <br />
+            We can ship to virtually any address in the world. Note that there
+            are restrictions on some products, and some products cannot be
+            shipped to international destinations. When you place an order, we
+            will estimate shipping and delivery dates for you based on the
+            availability of your items and the shipping options you choose.
+            Depending on the shipping provider you choose, shipping date
+            estimates may appear on the shipping quotes page. Please also note
+            that the shipping rates for many items we sell are weight-based. The
+            weight of any such item can be found on its detail page. To reflect
+            the policies of the shipping companies we use, all weights will be
+            rounded up to the next full pound.
           </p>
         </div>
       </div>
