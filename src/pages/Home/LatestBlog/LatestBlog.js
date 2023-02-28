9047d70fbddc35548ed4db5627e6d23e4f895ac5
Habidhossen <habidhossen2@gmail.com>
2023-02-28 15:39:58 +0600

    section title updated

--- src/pages/Home/LatestBlog/LatestBlog.js
+++ src/pages/Home/LatestBlog/LatestBlog.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import BlogCard from "../BlogCard/BlogCard";
 
 const LatestBlog = () => {
@@ -33,9 +35,22 @@ const LatestBlog = () => {
     },
   ];
   return (
-    <section className="my-20">
-      <h1 className="text-4xl font-extrabold text-center mb-6">Latest Blog</h1>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mx-16">
+    <section className="my-20 mx-20">
+      <div className="flex justify-between my-4">
+        <div>
+          <h1 className="text-xl font-bold">Our Latest News</h1>
+        </div>
+        <div>
+          <Link
+            to="/"
+            className="flex items-center gap-2 text-sm font-bold hover:text-primary transition duration-300"
+          >
+            View All <FaLongArrowAltRight />
+          </Link>
+        </div>
+      </div>
+      <hr className="mb-5" />
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {latestBlog
           .map((blog) => <BlogCard key={blog._id} blog={blog} />)
           .reverse()}
