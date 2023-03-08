14742aa82601eec4859a6aa1ec8c0af194f7fba6
Habidhossen <habidhossen2@gmail.com>
2023-03-09 01:58:51 +0600

    not found page updated

--- src/pages/Shared/NotFound/NotFound.js
+++ src/pages/Shared/NotFound/NotFound.js
@@ -5,16 +5,13 @@ import notFound from "../../../assets/images/not-found.png";
 const NotFound = () => {
   return (
     <section className="text-center flex flex-col items-center">
-      <img src={notFound} alt="" />
-      <h1 className="text-5xl font-extrabold uppercase">Oops!</h1>
-      <h5 className="text-xl font-semibold uppercase my-2">
+      <img className="w-96" src={notFound} alt="" />
+      <h1 className="text-6xl font-extrabold uppercase">Oops!</h1>
+      <h5 className="text-lg font-bold uppercase mt-2 mb-6">
         404 - Page not found
       </h5>
-      <p className="text-lg font-medium mb-4">
-        The page you're looking for isn't here.
-      </p>
-      <Link to="/" className="btn btn-outline btn-warning">
-        GO TO HOMEPAGE
+      <Link to="/" className="btn btn-primary text-white capitalize">
+        Go to Homepage
       </Link>{" "}
     </section>
   );
